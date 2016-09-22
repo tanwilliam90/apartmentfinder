dd503a04d3e0135e94e648df31238392c69f5408
Vivian Lin, William Tan <vivianlin.is@gmail.com>
2016-09-22 15:30:50 -0700

    adminify features

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'admin' => 'admin#index'
+  put 'admin/update/:id' => 'admin#update'
+  patch 'admin/update/:id' => 'admin#update'
+
   devise_for :users, controllers: { registrations: 'registrations', omniauth_callbacks: 'omniauth_callbacks' }
   resources :apartments do
     get 'map_location'
